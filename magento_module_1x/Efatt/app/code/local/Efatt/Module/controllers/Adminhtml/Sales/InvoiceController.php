0fdef1b63cd105fb14091ed93f0c10a054d67bcd
e.gennari <e.gennari@ellecisystem.it>
2018-07-16 15:09:25 +0200

    get invoice by id

--- magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
+++ magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
@@ -3,9 +3,14 @@ require_once Mage::getModuleDir('controllers', 'Mage_Adminhtml') . DS . 'Sales' 
 class Efatt_Module_Adminhtml_Sales_InvoiceController extends Mage_Adminhtml_Sales_InvoiceController
 {   
     public function viewinvoicespageAction() {
+
         $invoiceIds = $this->getRequest()->getParam('invoice_ids');
+
+        $invoice = Mage::getModel('sales/order_invoice')->load($invoiceIds);
        	
-       	echo $invoiceIds;
+       	echo "<pre>";
+       	var_dump($invoice);
+       	echo "</pre>";
     }
 }
 ?>
