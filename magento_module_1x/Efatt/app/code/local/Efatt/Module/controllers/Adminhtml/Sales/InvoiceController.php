13b3e0ace693e7e7deeffb428d7f087eeab1538d
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-07-17 10:32:05 +0200

    test

--- magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
+++ magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
@@ -1,4 +1,5 @@
 <?php
+//ric
 require_once Mage::getModuleDir('controllers', 'Mage_Adminhtml') . DS . 'Sales' . DS . 'InvoiceController.php';
 class Efatt_Module_Adminhtml_Sales_InvoiceController extends Mage_Adminhtml_Sales_InvoiceController
 {   
