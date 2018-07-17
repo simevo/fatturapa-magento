621558514ac4a86b237b2b52e7e6a12040ad5760
e.gennari <e.gennari@ellecisystem.it>
2018-07-17 09:17:33 +0200

    billing address

--- magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
+++ magento_module_1x/Efatt/app/code/local/Efatt/Module/controllers/Adminhtml/Sales/InvoiceController.php
@@ -12,6 +12,9 @@ class Efatt_Module_Adminhtml_Sales_InvoiceController extends Mage_Adminhtml_Sale
         /* estraggo l'oggetto order */
         $order = Mage::getModel('sales/order')->load($invoice->order_id);
 
+        /* estraggo il billing address */
+        $address = Mage::getModel('customer/address')->load($order->billing_address_id);
+
 		/* mostro per sviluppo i dati di entrambi gli oggettti */
         echo "<h1>Invoice data</h1>"; 
 
@@ -25,12 +28,20 @@ class Efatt_Module_Adminhtml_Sales_InvoiceController extends Mage_Adminhtml_Sale
        	var_dump($order);
        	echo "</pre>";
 
+       	echo "<h1>Billing address</h1>"; 
+
+        echo "<pre>";
+       	var_dump($address);
+       	echo "</pre>";
+
        	/* genero la fattura xml */
 
        	$xml = '<?xml version="1.0" encoding="UTF-8"?>';
        	$xml .= '<FatturaElettronicaHeader>';
        	$xml .= '<CedentePrestatore>';
         $xml .= '<DatiAnagrafici>';
+        $xml .= '<Anagrafica>';
+
 
 
 	    header('Content-type: text/xml');
