6fea1bd3357bb415d93d9f378d34909bc7b5e6df
e.gennari <e.gennari@ellecisystem.it>
2018-07-25 13:44:01 +0200

    added and included js for json/xml validation schema

---
+++ magento_module_1x/Efatt/app/code/local/Efatt/Module/Model/System/Config/Source/Regimefiscale.php
@@ -0,0 +1,34 @@
+<?php
+class Efatt_Module_Model_System_Config_Source_Regimefiscale 
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 'RF01', 'label' => Mage::helper('adminhtml')->__('RF01')),
+            array('value' => 'RF02', 'label' => Mage::helper('adminhtml')->__('RF02')),
+            array('value' => 'RF04', 'label' => Mage::helper('adminhtml')->__('RF04')),
+            array('value' => 'RF05', 'label' => Mage::helper('adminhtml')->__('RF05')),
+            array('value' => 'RF06', 'label' => Mage::helper('adminhtml')->__('RF06')),
+            array('value' => 'RF07', 'label' => Mage::helper('adminhtml')->__('RF07')),
+            array('value' => 'RF08', 'label' => Mage::helper('adminhtml')->__('RF08')),
+            array('value' => 'RF09', 'label' => Mage::helper('adminhtml')->__('RF09')),
+            array('value' => 'RF10', 'label' => Mage::helper('adminhtml')->__('RF10')),
+            array('value' => 'RF11', 'label' => Mage::helper('adminhtml')->__('RF11')),
+            array('value' => 'RF12', 'label' => Mage::helper('adminhtml')->__('RF12')),
+            array('value' => 'RF13', 'label' => Mage::helper('adminhtml')->__('RF13')),
+            array('value' => 'RF14', 'label' => Mage::helper('adminhtml')->__('RF14')),
+            array('value' => 'RF15', 'label' => Mage::helper('adminhtml')->__('RF15')),
+            array('value' => 'RF16', 'label' => Mage::helper('adminhtml')->__('RF16')),
+            array('value' => 'RF17', 'label' => Mage::helper('adminhtml')->__('RF17')),
+            array('value' => 'RF18', 'label' => Mage::helper('adminhtml')->__('RF18')),
+        );
+
+
+    }
+
+}
