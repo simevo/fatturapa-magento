9237b0962759170b89039ea68a95fd5fbda9c74a
e.gennari <e.gennari@ellecisystem.it>
2018-08-08 11:14:46 +0200

    test evidenziazione errori su campi da json (2)

--- test.php
+++ test.php
@@ -3,46 +3,6 @@
    "valid":false,
    "errors":[
       {
-         "property":"FatturaElettronica.versione",
-         "message":"does not match the regex pattern ^FP(A|R)12$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.ProgressivoInvio",
-         "message":"must be at least 1 characters long"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.FormatoTrasmissione",
-         "message":"does not match the regex pattern ^FP(A|R)12$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.CodiceDestinatario",
-         "message":"does not match the regex pattern ^[A-Z0-9]{6,7}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.TipoDocumento",
-         "message":"does not match the regex pattern ^TD[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.Numero",
-         "message":"must be at least 1 characters long"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[0].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[1].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[2].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
          "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].ImponibileImporto",
          "message":"does not match the regex pattern ^-?[0-9]{1,11}\\.[0-9]{2}$"
       },
@@ -53,7 +13,25 @@
    ]
 }</textarea>
 
+<input type="string" id="FatturaElettronica-FatturaElettronicaBody-0-DatiBeniServizi-DatiRiepilogo-0-Imposta">
+
+<input type="string" id="FatturaElettronica-FatturaElettronicaBody-0-DatiBeniServizi-DatiRiepilogo-0-ImponibileImporto">
+
 <script type="text/javascript">
-	console.log(document.getElementById("debug").innerHTML)
+	var json = document.getElementById("debug").innerHTML;
+	var obj = JSON.parse(json, function (key, value) {
+    
+		if(key == 'property') {
+			var elId = value;
+			elId = elId.replace(/\]./g, "-");
+			elId = elId.replace(/\[/g, "-");
+			elId = elId.replace(/\./g, "-");
+
+			document.getElementById(elId).style.border = "1px solid red";
+
+			console.log(elId);
+		}
+
+    });
 </script>
 
