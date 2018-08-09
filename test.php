032cb54df2953a02556e1396c7e77fc1fd165929
e.gennari <e.gennari@ellecisystem.it>
2018-08-09 09:40:22 +0200

    evidenziazione errori su campi da funzione validate json. Installazione su editinvoice.phtml (1)

--- test.php
+++ test.php
@@ -1,57 +1,5 @@
 
-<textarea id="debug">{
-   "valid":false,
-   "errors":[
-      {
-         "property":"FatturaElettronica.versione",
-         "message":"does not match the regex pattern ^FP(A|R)12$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.ProgressivoInvio",
-         "message":"must be at least 1 characters long"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.FormatoTrasmissione",
-         "message":"does not match the regex pattern ^FP(A|R)12$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.CodiceDestinatario",
-         "message":"does not match the regex pattern ^[A-Z0-9]{6,7}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.TipoDocumento",
-         "message":"does not match the regex pattern ^TD[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.Numero",
-         "message":"must be at least 1 characters long"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[0].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[1].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[2].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].AliquotaIVA",
-         "message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].ImponibileImporto",
-         "message":"does not match the regex pattern ^-?[0-9]{1,11}\\.[0-9]{2}$"
-      },
-      {
-         "property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].Imposta",
-         "message":"does not match the regex pattern ^-?[0-9]{1,11}\\.[0-9]{2}$"
-      }
-   ]
-}</textarea>
+<textarea id="debug">{"valid":false,"errors":[{"property":"FatturaElettronica.versione","message":"does not match the regex pattern ^FP(A|R)12$"},{"property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.ProgressivoInvio","message":"must be at least 1 characters long"},{"property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.FormatoTrasmissione","message":"does not match the regex pattern ^FP(A|R)12$"},{"property":"FatturaElettronica.FatturaElettronicaHeader.DatiTrasmissione.CodiceDestinatario","message":"does not match the regex pattern ^[A-Z0-9]{6,7}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.TipoDocumento","message":"does not match the regex pattern ^TD[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiGenerali.DatiGeneraliDocumento.Numero","message":"must be at least 1 characters long"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[0].AliquotaIVA","message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[1].AliquotaIVA","message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DettaglioLinee[2].AliquotaIVA","message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].AliquotaIVA","message":"does not match the regex pattern ^[0-9]{1,2}\\.[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].ImponibileImporto","message":"does not match the regex pattern ^-?[0-9]{1,11}\\.[0-9]{2}$"},{"property":"FatturaElettronica.FatturaElettronicaBody[0].DatiBeniServizi.DatiRiepilogo[0].Imposta","message":"does not match the regex pattern ^-?[0-9]{1,11}\\.[0-9]{2}$"}]}</textarea>
 
 <input type="string" id="FatturaElettronica-FatturaElettronicaBody-0-DatiBeniServizi-DatiRiepilogo-0-Imposta">
 <div id="FatturaElettronica-FatturaElettronicaBody-0-DatiBeniServizi-DatiRiepilogo-0-Imposta-error"></div>
